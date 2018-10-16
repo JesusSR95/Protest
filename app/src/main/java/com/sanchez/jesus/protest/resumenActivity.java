e1f0e56626134db8ed828f639aeaedb57860161f
Jesus Sanchez <js_ruiz@icloud.com>
2018-10-16 14:08:28 +0200

    Commit inicial

---
+++ app/src/main/java/com/sanchez/jesus/protest/resumenActivity.java
@@ -0,0 +1,58 @@
+package com.sanchez.jesus.protest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class resumenActivity extends AppCompatActivity {
+    private static final String TAG="InicioActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Mylog.d(TAG, "Iniciando OnCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumen);
+        Mylog.d(TAG, "Finalizando OnCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        Mylog.d(TAG, "Iniciando OnStart");
+        super.onStart();
+        Mylog.d(TAG, "Finalizando OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        Mylog.d(TAG, "Iniciando OnResume");
+        super.onResume();
+        Mylog.d(TAG, "Finalizando OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        Mylog.d(TAG, "Iniciando OnPause");
+        super.onPause();
+        Mylog.d(TAG, "Finalizando OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        Mylog.d(TAG, "Iniciando OnStop");
+        super.onStop();
+        Mylog.d(TAG, "Finalizando OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        Mylog.d(TAG, "Iniciando OnRestart");
+        super.onRestart();
+        Mylog.d(TAG, "Finalizando OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Mylog.d(TAG, "Iniciando OnDestroy");
+        super.onDestroy();
+        Mylog.d(TAG, "Finalizando OnDestroy");
+    }
+}
