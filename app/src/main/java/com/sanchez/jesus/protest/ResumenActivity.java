249c9f7cab534287c05954a14b970717c9f42f39
Jesus Sanchez <js_ruiz@icloud.com>
2018-10-19 10:36:23 +0200

    Añadido la activity acercade y menu de la activity resumen

--- app/src/main/java/com/sanchez/jesus/protest/ResumenActivity.java
+++ app/src/main/java/com/sanchez/jesus/protest/ResumenActivity.java
@@ -1,10 +1,40 @@
 package com.sanchez.jesus.protest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ResumenActivity extends AppCompatActivity {
     private static final String TAG="activity_splash_screen";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_resumen, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_nuevo:
+                Log.i("ActionBar", "Nuevo!");
+                return true;
+            case R.id.action_buscar:
+                Log.i("ActionBar", "Buscar!");;
+                return true;
+            case R.id.action_settings:
+                Log.i("ActionBar", "Settings!");
+                Intent it = new Intent(ResumenActivity.this, AcercaActivity.class);
+                startActivity(it);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
