c887d7daac7d3d694bcd18fdc835276f9ba0b873
Jesus Sanchez <js_ruiz@icloud.com>
2018-10-26 11:15:30 +0200

    Añadido los permisos de escritura en la activity anadir

--- app/src/main/java/com/sanchez/jesus/protest/Anadir.java
+++ app/src/main/java/com/sanchez/jesus/protest/Anadir.java
@@ -1,26 +1,33 @@
 package com.sanchez.jesus.protest;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
-
-import static android.app.ProgressDialog.show;
 
 public class Anadir extends AppCompatActivity {
 
     private static final String TAG="activity_splash_screen";
+    final private int CODE_WRITE_EXTERNAL_STORAGE_PERMISSION = 123;
+    private Context myContext;
+    private CoordinatorLayout coordinatorLayout;
+    EditText edtext1, edtext2, edtext3,  edtext4, edtext5;
+    Button bt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +39,70 @@ public class Anadir extends AppCompatActivity {
         String[] categoria = {"Montaje y mantenimiento de equipos","Redes locales","Aplicaciones ofimáticas","Sistemas operativos monopuesto","FOL"};
         spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categoria));
 
-        Button bt = (Button) findViewById(R.id.botonAceptar);
+        bt = (Button) findViewById(R.id.botonAceptar);
+        edtext1 = (EditText) findViewById(R.id.PreguntaeditText);
+        edtext2 = (EditText) findViewById(R.id.respuestaCorrecta);
+        edtext3 = (EditText) findViewById(R.id.respuestaIncorrecta1);
+        edtext4 = (EditText) findViewById(R.id.respuestaIncorrecta2);
+        edtext5 = (EditText) findViewById(R.id.respuestaIncorrecta3);
+
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "No se han rellenado todos los campos", Snackbar.LENGTH_LONG)
-                        .setActionTextColor(getResources().getColor(R.color.snackbar_action))
-                        .setAction("Accion", new View.OnClickListener() {
-                           @Override
-                           public void onClick(View v) {
-                               Log.i("snackbar", "Pulsada acción snackbar!");
-                           }
-                       })
-                .show();
+            public void onClick(View v) {
+                if (edtext1.getText().toString().isEmpty()) {
+                    Snackbar.make(v, "Rellenar todos los campos", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }else if(edtext2.getText().toString().isEmpty()){
+                    Snackbar.make(v, "Rellenar todos los campos", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }else if(edtext3.getText().toString().isEmpty()){
+                    Snackbar.make(v, "Rellenar todos los campos", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }else if(edtext4.getText().toString().isEmpty()){
+                    Snackbar.make(v, "Rellenar todos los campos", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }else if(edtext5.getText().toString().isEmpty()){
+                    Snackbar.make(v, "Rellenar todos los campos", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+                else{
+                    myContext = Anadir.this;
+                    coordinatorLayout = findViewById(R.id.coordinatorLayout);
+                    bt.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            int WriteExternalStoragePermission = ContextCompat.checkSelfPermission(myContext, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                            Log.d("MainActivity", "WRITE_EXTERNAL_STORAGE Permission: " + WriteExternalStoragePermission);
+
+                            if (WriteExternalStoragePermission != PackageManager.PERMISSION_GRANTED) {
+                                // Permiso denegado
+                                // A partir de Marshmallow (6.0) se pide aceptar o rechazar el permiso en tiempo de ejecución
+                                // En las versiones anteriores no es posible hacerlo
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                                    ActivityCompat.requestPermissions(Anadir.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, CODE_WRITE_EXTERNAL_STORAGE_PERMISSION);
+                                    // Una vez que se pide aceptar o rechazar el permiso se ejecuta el método "onRequestPermissionsResult" para manejar la respuesta
+                                    // Si el usuario marca "No preguntar más" no se volverá a mostrar este diálogo
+                                } else {
+                                    Snackbar.make(coordinatorLayout, getResources().getString(R.string.write_permiso_denegado), Snackbar.LENGTH_LONG)
+                                            .show();
+                                }
+                            } else {
+                                // Permiso aceptado
+                                Snackbar.make(coordinatorLayout, getResources().getString(R.string.write_permiso_aceptado), Snackbar.LENGTH_LONG)
+                                        .show();
+                            }
+
+                        }
+                    });
+                }
+
             }
         });
+}
 
-    }
 
     @Override
     protected void onStart() {
